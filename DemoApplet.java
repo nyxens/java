90ac99d79380795b36b418574c6211d5f626f28a
nyxens <devarshikrishna1@gmail.com>
2025-04-11 17:15:28 +0530

    es

--- DemoApplet.java
+++ DemoApplet.java
@@ -1,28 +1,28 @@
-import java.applet.*;
-import  java.awt.*;
-/*	
-<applet code=DemoApplet width=500 height=500> </applet>
-*/
-public class DemoApplet extends Applet
-{
-	public void init()
-	{
-		System.out.println("Applet Intialized");
-	}
-	public void start()
-	{
-	   System.out.println("Applet execution started");
-   }
-   public void paint(Graphics graphics)
-   {
-      graphics.drawString("hello",10, 40);
-   }
-   public void stop()
-   {
-	   System.out.println("Applet stopped");
-   }
-   public void destroy()
-   {
-	   System.out.println("Applet destroyed");
-   }
-   }
+// import java.applet.*;
+// import  java.awt.*;
+// /*	
+// <applet code=DemoApplet width=500 height=500> </applet>
+// */
+// public class DemoApplet extends Applet
+// {
+// 	public void init()
+// 	{
+// 		System.out.println("Applet Intialized");
+// 	}
+// 	public void start()
+// 	{
+// 	   System.out.println("Applet execution started");
+//    }
+//    public void paint(Graphics graphics)
+//    {
+//       graphics.drawString("hello",10, 40);
+//    }
+//    public void stop()
+//    {
+// 	   System.out.println("Applet stopped");
+//    }
+//    public void destroy()
+//    {
+// 	   System.out.println("Applet destroyed");
+//    }
+//    }
